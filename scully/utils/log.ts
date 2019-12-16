b99609e29bc428e16b7b640ca6197ee50b763fa7
Sander Elias <sanderelias+git@gmail.com>
2019-12-16 12:49:52 +0100

    fix for orange color

--- scully/utils/log.ts
+++ scully/utils/log.ts
@@ -1,9 +1,7 @@
 import chalk from 'chalk';
 
-
-const orange = chalk.hex('#FFA500')
-export const {white, red, yellow, green, organge}: {[x: string]: any} = chalk;
-
+export const orange = chalk.hex('#FFA500');
+export const {white, red, yellow, green}: {[x: string]: any} = chalk;
 
 export const log = (...a) => console.log(white(...a));
 export const logError = (...a) => console.log(red(...a));
