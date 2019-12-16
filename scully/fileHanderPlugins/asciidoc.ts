85b975653e57c5f2fd690d62bba8cda04bed0dcc
Sander Elias <sanderelias+git@gmail.com>
2019-12-16 11:52:45 +0100

    fix wrong version asciidoc.ts

--- scully/fileHanderPlugins/asciidoc.ts
+++ scully/fileHanderPlugins/asciidoc.ts
@@ -1,1 +1,12 @@
-z
+const asciidoctor = require('asciidoctor.js')();
+import {registerPlugin} from '../pluginManagement/pluginRepository';
+
+const asciiDocPlugin = async (raw: string) => {
+  return asciidoctor.convert(raw);
+};
+
+registerPlugin('fileHandler', 'adoc', {
+  alternateExtensions: ['asciidoc', 'asc'],
+  handler: asciiDocPlugin,
+});
+
