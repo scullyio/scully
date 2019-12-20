f35561071fce8eb086f78348a88a925431846adc
Sander Elias <SanderElias@users.noreply.github.com>
2019-12-20 12:57:50 +0100

    fix(ng-lib): correct httpclient import (#69)
    Instead of loading in the module in ng-lib get the httpclient dep
    
    closes #52

--- projects/scullyio/ng-lib/src/lib/components.module.ts
+++ projects/scullyio/ng-lib/src/lib/components.module.ts
@@ -1,10 +1,16 @@
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {ScullyContentComponent} from './scully-content/scully-content.component';
 
 @NgModule({
   declarations: [ScullyContentComponent],
-  imports: [HttpClientModule],
   exports: [ScullyContentComponent],
 })
-export class ComponentsModule {}
+export class ComponentsModule {
+  static forRoot() {
+    return {
+      NgModule: ComponentsModule,
+      deps: [HttpClient],
+    };
+  }
+}
