4ccf906ee428aac9eacef95fd661f30c310d7f72
Andrés Villanueva <andresvillanuevab@gmail.com>
2020-01-03 17:08:30 -0300

    style(scully): format with prettier

--- scully/routerPlugins/addOptionalRoutesPlugin.ts
+++ scully/routerPlugins/addOptionalRoutesPlugin.ts
@@ -4,21 +4,18 @@ import {RoutesTypes, RouteTypes} from '../utils/interfacesandenums';
 import {logError, yellow, logWarn} from '../utils/log';
 
 export const addOptionalRoutes = async (routeList = [] as string[]): Promise<HandledRoute[]> => {
-  const routesToGenerate = await routeList.reduce(
-    async (result: Promise<HandledRoute[]>, cur: string) => {
-      const x = await result;
-      if (scullyConfig.routes[cur]) {
-        const r = await routePluginHandler(cur);
-        x.push(...r);
-      } else if (cur.includes('/:')) {
-        logWarn(`No configuration for route "${yellow(cur)}" found. Skipping`);
-      } else {
-        x.push({route: cur, type: RouteTypes.default});
-      }
-      return x;
-    },
-    Promise.resolve([] as HandledRoute[])
-  );
+  const routesToGenerate = await routeList.reduce(async (result: Promise<HandledRoute[]>, cur: string) => {
+    const x = await result;
+    if (scullyConfig.routes[cur]) {
+      const r = await routePluginHandler(cur);
+      x.push(...r);
+    } else if (cur.includes('/:')) {
+      logWarn(`No configuration for route "${yellow(cur)}" found. Skipping`);
+    } else {
+      x.push({route: cur, type: RouteTypes.default});
+    }
+    return x;
+  }, Promise.resolve([] as HandledRoute[]));
 
   return routesToGenerate;
 };
@@ -44,11 +41,16 @@ async function routePluginHandler(route: string): Promise<HandledRoute[]> {
     return [{route, type: RouteTypes.default}];
   }
   if (plugins.router[conf.type]) {
-    const generatedRoutes = await (plugins.router[conf.type](route, conf) as unknown) as HandledRoute[];
-    generatedRoutes.forEach( handledRoute => {
+    const generatedRoutes = (await (plugins.router[conf.type](route, conf) as unknown)) as HandledRoute[];
+    generatedRoutes.forEach(handledRoute => {
       if (!handledRoute.route.startsWith('/')) {
-        // tslint:disable-next-line:max-line-length
-        logWarn(`The plugin '${conf.type}' needs to return handledRoutes with a route that starts with '/'. The route ${JSON.stringify(handledRoute)} is invalid.`);
+        logWarn(
+          `The plugin '${
+            conf.type
+          }' needs to return handledRoutes with a route that starts with '/'. The route ${JSON.stringify(
+            handledRoute
+          )} is invalid.`
+        );
       }
     });
     return generatedRoutes;
