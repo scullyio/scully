4c900d4322396228d457f291ba46549911d0c1bc
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-01-10 17:58:21 +0100

    refactor(scully): use context.logger instead of console.log

--- schematics/scully/src/ng-update/index.ts
+++ schematics/scully/src/ng-update/index.ts
@@ -1,22 +1,19 @@
-import {Rule, Tree} from '@angular-devkit/schematics';
-import chalk from 'chalk';
+import {Rule, Tree, SchematicContext} from '@angular-devkit/schematics';
 
 /** Entry point for the migration schematics with target of Angular v9 */
 export function updateToV0(): Rule {
-    return (tree: Tree) => {
-        console.log(tree);
-        return onMigrationComplete('V0', true);
-    };
+  return (tree: Tree, context: SchematicContext) => {
+    context.logger.info(JSON.stringify(tree, null, '  '));
+    return onMigrationComplete('V0', true, context);
+  };
 }
 /** Function that will be called when the migration completed. */
-function onMigrationComplete(targetVersion: string, hasFailures: boolean) {
-  console.log();
-  console.log(chalk.green(`  ✓  Updated Scully to ${targetVersion}`));
-  console.log();
+function onMigrationComplete(targetVersion: string, hasFailures: boolean, context: SchematicContext) {
+  context.logger.info(`✅ Updated Scully to ${targetVersion}`);
 
   if (hasFailures) {
-    console.log(chalk.yellow(
-      '  ⚠  Some issues were detected but could not be fixed automatically. Please check the ' +
-      'output above and fix these issues manually.'));
+    context.logger.warn(
+      'Some issues were detected but could not be fixed automatically. Please check the output above and fix these issues manually.'
+    );
   }
 }
