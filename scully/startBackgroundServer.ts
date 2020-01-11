893359602645128baf3681aed42de1c3c2474ef9
Sander Elias <sanderelias+git@gmail.com>
2020-01-11 10:14:47 +0100

    improvement(small refactor): refactor server so it doesn't collash anymore
    added better support for handling multiple servers, added better error messages.

---
+++ scully/startBackgroundServer.ts
@@ -0,0 +1,28 @@
+import {spawn} from 'child_process';
+import {existsSync} from 'fs-extra';
+import {join} from 'path';
+import {ScullyConfig} from './utils/interfacesandenums';
+import {logError, log, green} from './utils/log';
+
+export function startBackgroundServer(scullyConfig: ScullyConfig) {
+  const binary = ['/dist/scully/scully', '/node_modules/.bin/scully', '/node_modules/@scullyio/scully/scully']
+    .map(p => join(scullyConfig.homeFolder, p + '.js'))
+    .find(p => existsSync(p));
+
+  if (!binary) {
+    logError('Could not find scully binaries');
+    process.exit(15);
+    return;
+  }
+  console.log('starting', binary);
+  spawn('node', [binary, 'serve'], {
+    detached: true,
+    // stdio: 'inherit',
+  }).on('close', err => {
+    if (+err > 0) {
+      logError('Problem starting background servers', err);
+      process.exit(15);
+    }
+  });
+  log(` ${green('☺')}   Started servers in background`);
+}
