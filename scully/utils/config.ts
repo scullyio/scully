2514dd1ec7bb08c71af42e04d6919f14f2ea6458
Sander Elias <SanderElias@users.noreply.github.com>
2020-01-16 14:20:04 +0100

    improvement(cofig.ts): throw an error when `./dist` is used as outputpath in angular.json (#188)
    We will run into trouble when an user uses './dist` without subfolder. This PR does docuent that,
    and also make scully check, and throw en error when this is detected
    
    re #56

--- scully/utils/config.ts
+++ scully/utils/config.ts
@@ -29,6 +29,12 @@ const loadIt = async () => {
     // TODO: make scully handle other projects as just the default one.
     const defaultProject = angularConfig.defaultProject;
     distFolder = angularConfig.projects[defaultProject].architect.build.options.outputPath;
+    if (distFolder.endsWith('dist') && !distFolder.includes('/')) {
+      logError(
+        `Your distribution files are in "${yellow(distFolder)}". Please change that to include a subfolder`
+      );
+      process.exit(15);
+    }
   } catch (e) {
     logError(`Angular config file could not be parsed!`, e);
     process.exit(15);
@@ -51,7 +57,7 @@ const loadIt = async () => {
       staticport: /** 1771 */ 'scully'.split('').reduce((sum, token) => (sum += token.charCodeAt(0)), 1000),
       defaultPostRenderers: [],
       // tslint:disable-next-line:no-bitwise
-      hostUrl: compiledConfig.hostUrl
+      hostUrl: compiledConfig.hostUrl,
     }
     // compiledConfig
   ) as ScullyConfig;
