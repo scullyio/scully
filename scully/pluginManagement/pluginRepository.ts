699163ffb01c77219071c52c484440d3b6503f34
SanderElias <sanderelias+git@gmail.com>
2020-01-20 13:59:48 +0100

    feat(pluginrpository.ts): refactor registerplugin to take validator
    To make writing plugins easier, the validator is now a parameter to the registerPlugin function.
    Also the resiterPlugin functin now checks its types.

--- scully/pluginManagement/pluginRepository.ts
+++ scully/pluginManagement/pluginRepository.ts
@@ -35,12 +35,18 @@ export const registerPlugin = (
   type: PluginTypes,
   name: string,
   plugin: any,
+  validator = async () => [],
   {replaceExistingPlugin = false} = {}
 ) => {
+  if (!['router', 'render', 'fileHandler'].includes(type)) {
+    throw new Error(
+      `Type "${yellow(type)}" is not a known plugin type for registering plugin "${yellow(name)}"`
+    );
+  }
   if (replaceExistingPlugin === false && plugins[type][name]) {
     throw new Error(`Plugin ${name} already exists`);
   }
-  if (type === 'router' && plugin[configValidator] === undefined) {
+  if (type === 'router' && configValidator === undefined) {
     logError(`
 ---------------
    Route plugin "${yellow(name)}" should have an config validator attached to '${
@@ -50,5 +56,6 @@ export const registerPlugin = (
 `);
     plugin[configValidator] = async () => [];
   }
+  plugin[configValidator] = validator;
   plugins[type][name] = plugin;
 };
