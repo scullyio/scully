afb8e20203cce0b2f049014859d47801c9d49c54
SanderElias <sanderelias+git@gmail.com>
2020-01-27 13:34:28 +0100

    fix(appp.module): put browsermodule back in
    was a bit too eager to remove things

--- projects/sampleBlog/src/app/app.module.ts
+++ projects/sampleBlog/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {ScullyLibModule} from '@scullyio/ng-lib';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [HttpClientModule, AppRoutingModule, ScullyLibModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, ScullyLibModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
