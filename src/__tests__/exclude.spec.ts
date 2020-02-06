26570c0ca68591c6652dff27e36151bf713a19a7
Jorge Cano <jorgeucano@gmail.com>
2020-02-06 11:00:01 -0300

    feat(husky): add open server

--- src/__tests__/exclude.spec.ts
+++ src/__tests__/exclude.spec.ts
@@ -2,7 +2,7 @@ import {readFileSync} from 'fs';
 import {join} from 'path';
 
 describe('guessParserOptions', () => {
-  it("should remove files from route discovery so that routes aren't present", () => {
+  it('should remove files from route discovery so that routes aren\'t present', () => {
     let fileExists = true;
     // Trying to read the missing file should throw an error. If error doesn't happen, test fails. Meaning...
     // if the files exists (which it shouldn't, the test fails.
