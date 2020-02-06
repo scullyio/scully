94e275c96328a824671350b0830f3a8a9234f812
Aaron Frost <aaronfrost@gmail.com>
2020-02-05 23:10:20 -0700

    Frosty/excluded files (#280)
    * feat(excludedfiles): added guessParserOptions to scully.config
    
    Added ability to pass guessParserOptions to scully.config. Currently only supports excludedFiles so
    that you can remove files from guessParsers scan for routes. Also added some tests around
    extraRoutes, excludedFiles, and moved and improved the transferState tests.
    
    * fix(excludedfiles): fixed path for sampleBlog
    
    * Fix the docs for guessParserOptions

--- scully.sampleBlog.config.js
+++ scully.sampleBlog.config.js
@@ -1,3 +1,4 @@
+const {join} = require('path');
 /** load the plugin  */
 require('./extraPlugin/extra-plugin.js');
 require('./extraPlugin/tocPlugin');
@@ -7,8 +8,8 @@ exports.config = {
   /** outDir is where the static distribution files end up */
   outDir: './dist/static',
   // hostName: '0.0.0.0',
-  hostUrl: 'http://localHost:5000',
-  extraRoutes: [''],
+  // hostUrl: 'http://localHost:5000',
+  extraRoutes: ['/exclude/present'],
   routes: {
     '/demo/:id': {
       type: 'extra',
@@ -70,5 +71,8 @@ exports.config = {
     '/**': {
       type: 'ignored',
     },
+  },
+  guessParserOptions: {
+    excludedFiles: ['projects/sampleBlog/src/app/exclude/exclude-routing.module.ts'],
   },
 };
