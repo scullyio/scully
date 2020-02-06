723e50b339139ffcb78bf56c0a0113476c03097c
sanderelias <sanderelias+git@gmail.com>
2020-02-06 15:39:01 +0100

    fix(traverseapp): looks in the different places for tsconfig.app.json
    closes 279

---
+++ scully/utils/cli-options.ts
@@ -0,0 +1,19 @@
+import * as yargs from 'yargs';
+
+export const {watch} = yargs
+  .boolean('wm')
+  .default('wm', false)
+  .alias('wm', 'watch')
+  .describe('wm', 'Use this flag for use the watch mode into scully').argv;
+
+export const {removeStaticDist} = yargs
+  .boolean('RSD')
+  .default('RSD', false)
+  .alias('RSD', 'removeStaticDist')
+  .describe('RSD', 'Use this flag to remove the Scully outfolder before starting').argv;
+
+export const {argv: options} = yargs.option('port', {
+  alias: 'p',
+  type: 'number',
+  description: 'The port to run on',
+});
