f13aa826fe7dd16353d55f6d47214a77dbddd5e8
jorgeucano <jorgeucano@gmail.com>
2020-02-07 09:32:48 -0300

    fix(spec): fix path for exclude and extraRoute in test for support unix and windows

--- src/__tests__/exclude.spec.ts
+++ src/__tests__/exclude.spec.ts
@@ -2,19 +2,16 @@ import {readFileSync} from 'fs';
 import {join} from 'path';
 
 describe('guessParserOptions', () => {
-  it('should remove files from route discovery so that routes aren\'t present', () => {
+  it("should remove files from route discovery so that routes aren't present", () => {
     let fileExists = true;
     // Trying to read the missing file should throw an error. If error doesn't happen, test fails. Meaning...
     // if the files exists (which it shouldn't, the test fails.
     try {
       readFileSync(join(__dirname, '../../dist/static/exclude/notpresent/index.html'), 'UTF8');
     } catch ({message, code, path}) {
-      if (code === 'ENOENT' && path &&
-        ( path.endsWith('/notpresent/index.html')
-          || path.endsWith('\\\\notpresent\\\\index.html'))
-      ) {
+      if (code === 'ENOENT' && path && path.endsWith(`${path.sep}notpresent${path.sep}index.html`)) {
+        fileExists = false;
       }
-      fileExists = false;
     }
     expect(fileExists).toBe(false);
   });
