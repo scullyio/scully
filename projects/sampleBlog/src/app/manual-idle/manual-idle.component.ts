1dc5e4e0178fc2a139d017c1efece4054e65fc2a
Sander Elias <sanderelias+git@gmail.com>
2020-03-03 10:07:18 +0100

    test(monorepo): create test for manual idle

--- projects/sampleBlog/src/app/manual-idle/manual-idle.component.ts
+++ projects/sampleBlog/src/app/manual-idle/manual-idle.component.ts
@@ -1,4 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {IdleMonitorService} from '@scullyio/ng-lib';
+
+declare global {
+  interface Window {
+    scullyManualIdle: boolean;
+  }
+}
 
 @Component({
   selector: 'app-manual-idle',
@@ -6,7 +13,12 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./manual-idle.component.css'],
 })
 export class ManualIdleComponent implements OnInit {
-  constructor() {}
+  text = 'this text is displayed by automated detection';
 
-  ngOnInit(): void {}
+  constructor(private ims: IdleMonitorService) {}
+
+  ngOnInit(): void {
+    setTimeout(() => (this.text = 'manualIdle'), 10 * 1000);
+    setTimeout(() => this.ims.fireManualMyAppReadyEvent(), 11 * 1000);
+  }
 }
