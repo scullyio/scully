b926a5b5b4f37b696af6d6d4a9115f1cc09e482d
frosty <aaronfrost@gmail.com>
2020-03-02 22:54:41 -0700

    test(lib): added tests for flash prevention
    I added a flash prevention test.

--- projects/sampleBlog/src/app/slow/slow.component.ts
+++ projects/sampleBlog/src/app/slow/slow.component.ts
@@ -1,19 +1,18 @@
 import {HttpClient} from '@angular/common/http';
 import {Component} from '@angular/core';
-import {isScullyGenerated, isScullyRunning} from '@scullyio/ng-lib';
+import {isScullyGenerated} from '@scullyio/ng-lib';
 import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-slow',
   template: `
     <p>slow works!</p>
-    <h1 *ngIf="isNotGenerated">Not Scully Generated</h1>
+    <h1 *ngIf="!isGenerated">Scully Not Generated</h1>
     <h1 *ngIf="isGenerated">Scully Generated</h1>
   `,
   styles: [``],
 })
 export class SlowComponent {
-  isNotGenerated = isScullyRunning() || (!isScullyGenerated() && !isScullyRunning());
   isGenerated = isScullyGenerated();
 
   delay$ = this.http.get('http://localhost:8200/slow/4000');
