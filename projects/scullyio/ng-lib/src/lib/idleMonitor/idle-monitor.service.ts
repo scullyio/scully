b45b06ceef064b4afb54e3228a20d5282f58da64
Sander Elias <SanderElias@users.noreply.github.com>
2020-03-05 13:10:21 +0100

    feat(scully): meta tag added (#355)
    Meta tag added, SculluIO gloabal variable spelling the same everywhere, added exposeToPage and
    ijectToPage, updated library to cater for thsethings

--- projects/scullyio/ng-lib/src/lib/idleMonitor/idle-monitor.service.ts
+++ projects/scullyio/ng-lib/src/lib/idleMonitor/idle-monitor.service.ts
@@ -17,7 +17,11 @@ interface LocalState {
 
 declare global {
   interface Window {
-    'ScullyIO-ManualIdle': boolean;
+    'ScullyIO-exposed': {
+      manualIdle?: boolean;
+      transferState?: any;
+      [key: string]: any;
+    };
   }
 }
 
@@ -52,6 +56,8 @@ export class IdleMonitorService {
   ) {
     /** provide the default for missing conf paramter */
     this.scullyLibConfig = Object.assign({}, ScullyDefaultSettings, conf);
+    const exposed = window['ScullyIO-exposed'] || {};
+    const manualIdle = !!exposed.manualIdle;
 
     if (
       !this.scullyLibConfig.manualIdle &&
@@ -63,9 +69,7 @@ export class IdleMonitorService {
         .pipe(
           filter(ev => ev instanceof NavigationEnd && ev.urlAfterRedirects !== undefined),
           /** don't check the page that has this setting. event is only importand on page load */
-          filter((ev: NavigationEnd) =>
-            window['ScullyIO-ManualIdle'] ? ev.urlAfterRedirects !== this.initialUrl : true
-          ),
+          filter((ev: NavigationEnd) => (manualIdle ? ev.urlAfterRedirects !== this.initialUrl : true)),
           tap(() => this.zoneIdleCheck())
         )
         .subscribe();
