45db89e1a941e7341961411a75f143358504ece7
jorgeucano <jorgeucano@gmail.com>
2020-03-07 01:09:59 -0300

    feat(playground): add more buttons options

--- projects/scullyDocs/src/app/components/button/button.component.sandbox.ts
+++ projects/scullyDocs/src/app/components/button/button.component.sandbox.ts
@@ -1,8 +1,25 @@
 import {sandboxOf} from 'angular-playground';
 import {ButtonComponent} from './button.component';
 
-export default sandboxOf(ButtonComponent).add('default', {
-  template: `<nav>
-                <app-button [text]="'GET STARTED'" [class]="''" [link]="'/blah'" [active]="true"></app-button>
-              </nav>`,
-});
+export default sandboxOf(ButtonComponent)
+  .add('default', {
+    template: `<app-button [text]="'Default'" [class]="''" [link]="'/blah'"></app-button>`,
+  })
+  .add('nav-select-active', {
+    template: `<nav>
+                <app-button [text]="'GET STARTED'" [class]="'router-link-active'" [link]="'/blah'" [active]="true"></app-button>
+             </nav>`,
+  })
+  .add('nav-default-inactive', {
+    template: `<nav>
+                <app-button [text]="'Features'" [class]="''" [link]="'/blah'" [active]="false"></app-button>
+             </nav>`,
+  })
+  .add('full-menu', {
+    template: `
+             <nav>
+                <app-button [text]="'GET STARTED'" [class]="'router-link-active'" [link]="'/blah'" [active]="true"></app-button>
+                <app-button [text]="'Features'" [class]="''" [link]="'/blah'" [active]="false"></app-button>
+             </nav>
+    `,
+  });
