2dec18ec9ff77a7da8461a1369a588a2d1e1815f
Israel Guzman <iguzman@dealersocket.com>
2020-03-07 10:20:25 -0600

    Adds header, text and icon styles to the card

--- projects/scullyDocs/src/app/components/card/card.component.sandbox.ts
+++ projects/scullyDocs/src/app/components/card/card.component.sandbox.ts
@@ -1,6 +1,36 @@
 import {sandboxOf} from 'angular-playground';
 import {CardComponent} from './card.component';
 
-export default sandboxOf(CardComponent).add('default', {
-  template: `<app-card></app-card>`,
-});
+export default sandboxOf(CardComponent)
+  .add('default', {
+    template: `<app-card
+                    [header]=" 'Machine Learning Driven' "
+                    [text]=" 'Scully uses machine learning techniques to find all of your app project routes and pre-render each page to plain HTML & CSS.' "
+                    [iconUrl]=" 'assets/angular.svg' "
+                    [alt]=" 'Angular logo' ">
+                </app-card>`,
+  })
+  .add('png icon', {
+    template: `<app-card
+                    [header]=" 'Machine Learning Driven' "
+                    [text]=" 'Scully uses machine learning techniques to find all of your app project routes and pre-render each page to plain HTML & CSS.' "
+                    [iconUrl]=" 'assets/angular-original.png' "
+                    [alt]=" 'Angular logo' ">
+                </app-card>`,
+  })
+  .add('svg external icon', {
+    template: `<app-card
+                    [header]=" 'Machine Learning Driven' "
+                    [text]=" 'Scully uses machine learning techniques to find all of your app project routes and pre-render each page to plain HTML & CSS.' "
+                    [iconUrl]=" 'https://angular.io/assets/images/logos/angular/angular_solidBlack.svg' "
+                    [alt]=" 'Angular logo' ">
+                </app-card>`,
+  })
+  .add('png external icon', {
+    template: `<app-card
+                    [header]=" 'Machine Learning Driven' "
+                    [text]=" 'Scully uses machine learning techniques to find all of your app project routes and pre-render each page to plain HTML & CSS.' "
+                    [iconUrl]=" 'https://angular.io/assets/images/logos/angular/angular.png' "
+                    [alt]=" 'Angular logo' ">
+                </app-card>`,
+  });
