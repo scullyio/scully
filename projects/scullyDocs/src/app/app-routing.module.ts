67fc2e5f7feb4a343828519709c0e3e386b47404
jorgeucano <jorgeucano@gmail.com>
2020-03-10 09:01:08 -0300

    feat(docsWeb): add menu

--- projects/scullyDocs/src/app/app-routing.module.ts
+++ projects/scullyDocs/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
-  {path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)},
+  {path: '', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)},
 ];
 
 @NgModule({
