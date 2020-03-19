2f39b2463a3cc214cf3da20788f0cd897d4039f2
Sander Elias <SanderElias@users.noreply.github.com>
2020-03-19 12:42:57 +0100

    feat(scully): support for routeProccesingDone and allDone plugins (#390)
    * feat(scully): support for routeProccesingDone and allDOne plugins
    
    * fix(scully): wait a bit longer for the server
    
    * fix(scully): add missing performanceHooks import

---
+++ scully/utils/handlers/handleRouteDiscoveryDone.ts
@@ -0,0 +1,14 @@
+import {performance} from 'perf_hooks';
+import {plugins} from '../../pluginManagement/pluginRepository';
+import {HandledRoute} from '../../routerPlugins/addOptionalRoutesPlugin';
+import {deepClone} from '../deepClone';
+import {performanceIds} from '../performanceIds';
+
+export async function handleRouteDiscoveryDone(handledRoutes: HandledRoute[]) {
+  /** protect from unwanted behavior */
+  performance.mark('startRouteDonePlugins');
+  performanceIds.add('RouteDonePlugins');
+  const clone = deepClone(handledRoutes);
+  await Promise.all(Object.values(plugins.routeDiscoveryDone).map(plugin => plugin(clone)));
+  performance.mark('stopRouteDonePlugins');
+}
