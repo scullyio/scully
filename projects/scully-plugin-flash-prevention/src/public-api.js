480e313cb820a323e42284937b88c284a2adaac8
Aaron Frost <aaronfrost@gmail.com>
2020-03-26 17:27:26 -0600

    feat(scully): add Flash Prevention Plugin (#418)
    * feat(scully): add Flash Prevention Plugin
    
    Initial add of flash prevention plugin
    
    * fix(lib): fixing the build around flash-prevention-plugin
    
    This build needed to be more tolerant of our shitty system. So I added a build script affordance for
    it.
    
    Co-authored-by: Jorge Cano <jorgeucano@gmail.com>

--- projects/scully-plugin-flash-prevention/src/public-api.js
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of scully-plugin-flash-prevention
- */
-
-const {getDiscountFlashPreventionPlugin} = require('./lib/flash-prevention.plugin');
-
-module.exports = {getDiscountFlashPreventionPlugin};
