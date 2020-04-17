d0f2857b803e33728e6ad2f751d4bc0cb9305ea6
sanderelias <sanderelias+git@gmail.com>
2020-04-17 11:12:03 +0200

    feat(scully): add a flag to make scanning for package.json befoer angular.json in momorepo

--- scully/utils/compileConfig.ts
+++ scully/utils/compileConfig.ts
@@ -32,6 +32,7 @@ export const compileConfig = async (): Promise<ScullyConfig> => {
     The config file should have a name that is formated as:
        scully.${yellow('<projectName>')}.config.js
     where ${yellow('<projectName>')} is the name of the project as defined in the 'angular.json' file
+    When you are in a mixed mono-repo you might need to use the --pjFirst flag.
 ---------
 `);
       return ({projectName: project || defaFaultProjectName} as unknown) as ScullyConfig;
