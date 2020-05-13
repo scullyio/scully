fd1ffca8d5c6079933192c2a98d8d22cfe8894b6
sanderelias <sanderelias+git@gmail.com>
2020-05-13 15:06:22 +0200

    test(monorepo): updated snapshots without dev stiff

--- scully.scullyDocs.config.js
+++ scully.scullyDocs.config.js
@@ -1,9 +1,8 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-require('./plugins/demos/docs-link-update');
+require('./demos/plugins/docs-link-update');
 var scully_1 = require('@scullyio/scully');
 scully_1.setPluginConfig('md', { enableSyntaxHighlighting: true });
-require('./plugins/demos/docs-link-update');
 exports.config = {
   projectRoot: './ng-projects/scullyDocs/src',
   projectName: 'scullyDocs',
