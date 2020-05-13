23c45c2f269cc0f3a830fe47c68b200034f9237d
sanderelias <sanderelias+git@gmail.com>
2020-05-13 14:52:00 +0200

    fix(monorepo): fix tests

--- scully.scullyDocs.config.ts
+++ scully.scullyDocs.config.ts
@@ -1,9 +1,8 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-require('./plugins/demos/docs-link-update');
-var scully_1 = require('@scullyio/scully');
-scully_1.setPluginConfig('md', { enableSyntaxHighlighting: true });
-require('./plugins/demos/docs-link-update');
+import './demos/plugins/docs-link-update';
+import { setPluginConfig } from '@scullyio/scully';
+
+setPluginConfig('md', { enableSyntaxHighlighting: true });
+
 exports.config = {
   projectRoot: './ng-projects/scullyDocs/src',
   projectName: 'scullyDocs',
