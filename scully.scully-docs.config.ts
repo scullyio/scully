df215607448b29277dc7932689e8fb565eb6ae22
sanderelias <sanderelias+git@gmail.com>
2020-05-30 05:51:05 +0200

    feat(docsWeb): remove angular from docs pages

--- scully.scully-docs.config.ts
+++ scully.scully-docs.config.ts
@@ -1,12 +1,14 @@
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { DisableAngular } from 'scully-plugin-disable-angular';
 import './demos/plugins/docs-link-update';
-import { setPluginConfig } from '@scullyio/scully';
 
 setPluginConfig('md', { enableSyntaxHighlighting: true });
 
-exports.config = {
+export const config: ScullyConfig = {
   projectRoot: './apps/scully-docs/src',
   projectName: 'scully-docs',
   outDir: './dist/static/doc-sites',
+  defaultPostRenderers: [DisableAngular],
   routes: {
     '/docs/:slug': {
       type: 'contentFolder',
