1fda81b0f2f84d8c41f7789eacea1fd03fc662d9
Sander Elias <SanderElias@users.noreply.github.com>
2020-06-01 15:46:00 +0200

    sander/betterLogs (#597)
    * feat(scully): display relative instead of full paths in logs
    
    * feat(scully): write logs to `scully.log` filtered on severity

--- scully.sampleBlog.config.ts
+++ scully.sampleBlog.config.ts
@@ -27,11 +27,7 @@ export const config: ScullyConfig = {
   // hostUrl: 'http://localHost:5000',
   // extraRoutes: Promise.resolve(['/exclude/present']),
   extraRoutes: new Promise(r => {
-    console.log('this line should be visible for 15 seconds');
-    setTimeout(() => {
-      console.log('done waiting');
-      r(['/exclude/present']);
-    }, 15000);
+    r(['/exclude/present']);
   }),
   /** Use only inlined HTML, no data.json will be written/read */
   // inlineStateOnly: true,
