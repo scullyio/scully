679638fb8dde82da07a7a7627474ca693693d39f
sanderelias <sanderelias+git@gmail.com>
2020-06-01 10:53:21 +0200

    feat(scully): display relative instead of full paths in logs

--- libs/scully/src/lib/utils/log.ts
+++ libs/scully/src/lib/utils/log.ts
@@ -1,8 +1,22 @@
 import chalk from 'chalk';
+import { scullyConfig } from '../utils/config';
 
 export const orange = chalk.hex('#FFA500');
 export const { white, red, yellow, green }: { [x: string]: any } = chalk;
 
-export const log = (...a) => console.log(white(...a));
+export const log = (...a) => myLog(white(...a));
 export const logError = (...a) => console.log(red(...a));
 export const logWarn = (...a) => console.log(orange(...a));
+
+function myLog(...args: any[]) {
+  const out = [];
+  for (const item of args) {
+    out.push(typeof item === 'string' ? makeRelative(item) : item);
+  }
+  console.log(...out);
+}
+
+function makeRelative(txt: string) {
+  const h = scullyConfig?.homeFolder || process.cwd();
+  return txt.replace(h, '.');
+}
