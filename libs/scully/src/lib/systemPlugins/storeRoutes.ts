fdd23a861d50f01fad37267a1a02abc488c3d791
sanderelias <sanderelias+git@gmail.com>
2020-06-11 09:24:19 +0200

    refactor(scully): move the cached routeData to its proper place
    better cache, prep for better speed

--- libs/scully/src/lib/systemPlugins/storeRoutes.ts
+++ libs/scully/src/lib/systemPlugins/storeRoutes.ts
@@ -1,10 +1,10 @@
 import { writeFileSync } from 'fs';
 import { join } from 'path';
 import { HandledRoute } from '../routerPlugins/addOptionalRoutesPlugin';
+import { watch } from '../utils/cli-options';
 import { scullyConfig } from '../utils/config';
 import { createFolderFor } from '../utils/createFolderFor';
-import { log, logError, yellow } from '../utils/log';
-import { watch } from '../utils/cli-options';
+import { log, logError, logWarn, yellow } from '../utils/log';
 
 const routesFileName = '/assets/scully-routes.json';
 
@@ -13,7 +13,7 @@ export async function storeRoutes(routes: HandledRoute[]) {
     /** in the scully outfolder */
     join(scullyConfig.outDir, routesFileName),
     /** in the angular dist folder */
-    join(scullyConfig.homeFolder, scullyConfig.distFolder, routesFileName)
+    join(scullyConfig.homeFolder, scullyConfig.distFolder, routesFileName),
   ];
   if (!watch) {
     /** in the angular source folder */
@@ -21,24 +21,32 @@ export async function storeRoutes(routes: HandledRoute[]) {
       /** in the angular source folder */
       join(scullyConfig.homeFolder, scullyConfig.sourceRoot, routesFileName)
     );
+  } else {
+    logWarn(
+      `running in watch-mode, routefile in source assets will not be updated`
+    );
   }
   try {
     const jsonResult = JSON.stringify(
-      routes.map(r => ({ route: r.route || '/', title: r['title'], ...r.data }))
+      routes.map((r) => ({
+        route: r.route || '/',
+        title: r.title,
+        ...r.data,
+      }))
     );
-    const write = file => {
+    const write = (file) => {
       createFolderFor(file);
       writeFileSync(file, jsonResult);
     };
     files.forEach(write);
     log(`Route list created in files:${files.map(
-      f => `
+      (f) => `
   "${yellow(f)}"`
     )}
 `);
   } catch {
     logError(`Could not write routes to files:${files.map(
-      f => `
+      (f) => `
   "${yellow(f)}"`
     )}
 `);
