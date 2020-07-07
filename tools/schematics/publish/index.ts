e60b1af87e5cbd790df6663cb670f4aa9f41d04b
jorgeucano <jorgeucano@gmail.com>
2020-07-07 10:01:48 -0300

    feat(monorepo): add automatic update&publish

--- tools/schematics/publish/index.ts
+++ tools/schematics/publish/index.ts
@@ -1,25 +1,57 @@
 import {
-  apply,
-  chain,
-  mergeWith,
-  move,
+  empty,
   Rule,
   SchematicContext,
   Tree,
-  url,
-  externalSchematic,
 } from '@angular-devkit/schematics';
+const { exec } = require('child_process');
+
+const _path = 'dist/libs/';
 
 export default function (schema: any): Rule {
   return (tree: Tree, context: SchematicContext) => {
     // @ts-ignore
-    return chain([modifyPackage(schema), publishPackages(schema)])(
-      tree,
-      context
-    );
+    return empty([modifyPackage(schema, tree)])(tree, context);
   };
 }
 
-function modifyPackage(schema) {}
+function modifyPackage(schema, tree) {
+  const forPublish = [];
+  if (schema['scully']) {
+    forPublish.push('scully');
+  }
+  if (schema['ng-lib']) {
+    forPublish.push('ng-lib');
+  }
+  if (schema['scully-schematics']) {
+    forPublish.push('ng-lib');
+  }
+  updatePackages(forPublish, schema.type);
+  return tree;
+}
 
-function publishPackages(schema) {}
+function updatePackages(forPublish, type) {
+  forPublish.forEach((_package) => {
+    exec(
+      `npm version ${type} --prefix ${_path}${_package} &&
+                      npm version ${type} --prefix libs/${_package}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`Package ${_package} v:${stdout}`);
+        exec(
+          `npm publish --access publish --prefix ${_path}${_package}`,
+          (_error, _stdout, _stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              return;
+            }
+            console.log(`Package ${_package} v:${stdout}`);
+          }
+        );
+      }
+    );
+  });
+}
