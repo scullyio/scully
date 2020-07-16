a9939038ce3df105c31100f4bdbd58d6202fb50e
Gérôme Grignon <32737308+geromegrignon@users.noreply.github.com>
2020-07-16 05:57:21 +0200

    feature(schematics): remove boilerplate (#747)
    remove boilerplate on blog support

--- libs/scully-schematics/src/files/blog-module/blog.component.ts
+++ libs/scully-schematics/src/files/blog-module/blog.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-
-declare var ng: any;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css'],
-  preserveWhitespaces: true,
-  encapsulation: ViewEncapsulation.Emulated
 })
 export class BlogComponent implements OnInit {
   ngOnInit() {}
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor() {}
 }
