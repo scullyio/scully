014a4b9e3aaf732e1e76e1e382b5921b24767481
Sander Elias <SanderElias@users.noreply.github.com>
2020-07-17 14:15:54 +0200

    sander/PluginNames (#755)
    * feat(schematics): update schematic to stop using latest
    
    * WIP: sync all the versions of packages
    
    * fix(monorepo): fix naming plugins
    
    * refactor(monorepo): back to orignal versions
    
    Co-authored-by: jorgeucano <jorgeucano@gmail.com>

---
+++ libs/plugins/scully-plugin-remove-scripts/src/lib/plugins-scully-plugin-remove-scripts.spec.ts
@@ -0,0 +1,7 @@
+import { pluginsScullyPluginRemoveScripts } from './plugins-scully-plugin-remove-scripts';
+
+describe('pluginsScullyPluginRemoveScripts', () => {
+  it('should work', () => {
+    expect(pluginsScullyPluginRemoveScripts()).toEqual('plugins-scully-plugin-remove-scripts');
+  });
+});
