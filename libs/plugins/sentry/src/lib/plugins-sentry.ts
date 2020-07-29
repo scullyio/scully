06e150efea914a286c3989f072ecd6f3fdab0a5d
PatrickJS <github@gdi2290.com>
2020-07-28 20:48:53 -0700

    feat: angular providers

--- libs/plugins/sentry/src/lib/plugins-sentry.ts
+++ libs/plugins/sentry/src/lib/plugins-sentry.ts
@@ -8,24 +8,27 @@ export const sentryPlugin = async (html: string): Promise<string> => {
   if (!sentryConfig) {
     throw new Error('sentry plugin missing configuration');
   }
-
-  const key = sentryConfig['key'];
-  const org = sentryConfig['org'];
-  const project = sentryConfig['project'];
-
   const src = sentryConfig['src'] || 'https://browser.sentry-cdn.com/5.20.1/bundle.min.js';
   const integrity = sentryConfig['integrity'] || 'sha384-O8HdAJg1h8RARFowXd2J/r5fIWuinSBtjhwQoPesfVILeXzGpJxvyY/77OaPPXUo';
   const crossorigin = sentryConfig['crossorigin'] === false ? '' : 'anonymous';
+  let dsn = sentryConfig['dsn'];
+  if (!dsn) {
+    const key = sentryConfig['key'];
+    const org = sentryConfig['org'];
+    const project = sentryConfig['project'];
 
-  if (!key) {
-    throw new Error('sentry plugin "key" missing configuration');
+    if (!key) {
+      throw new Error('sentry plugin "key" missing configuration');
+    }
+    if (!org) {
+      throw new Error('sentry plugin "org" missing configuration');
+    }
+    if (!project) {
+      throw new Error('sentry plugin "project" missing configuration');
+    }
+    dsn = `https://${key}@${org}.ingest.sentry.io/${project}`;
   }
-  if (!org) {
-    throw new Error('sentry plugin "org" missing configuration');
-  }
-  if (!project) {
-    throw new Error('sentry plugin "project" missing configuration');
-  }
+
   if (!src) {
     throw new Error('sentry plugin "src" missing configuration');
   }
@@ -35,7 +38,7 @@ export const sentryPlugin = async (html: string): Promise<string> => {
 
   const sentryScript = `
     <script src="${src}" integrity="${integrity}" ${crossorigin}></script>
-    <script>window.Sentry && window.Sentry.init({ dsn: 'https://${key}@${org}.ingest.sentry.io/${project}>' });</script>`;
+    <script>window.Sentry && window.Sentry.init({ dsn: '${dsn}', integrations: [new window.Sentry.Integrations.TryCatch({ XMLHttpRequest: false })] });</script>`;
 
   return html.replace(/<\/head/i, `${sentryScript}</head`);
 };
