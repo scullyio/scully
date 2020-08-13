ed10cedbfe876307eaf261bb52174467dc10b6bb
sanderelias <sanderelias+git@gmail.com>
2020-08-13 16:19:17 +0200

    feat(scully): an option to use external sites as the ource for puppeteer.
    this options enables you to get content of an external site. the page on the location should fire
    off the AngularReady event when its idle

--- libs/scully/src/lib/utils/serverstuff/dataServer.ts
+++ libs/scully/src/lib/utils/serverstuff/dataServer.ts
@@ -12,18 +12,29 @@ export async function startDataServer(ssl: boolean) {
 
     dataServer.use(function (req, res, next) {
       res.header('Access-Control-Allow-Origin', `${req.get('origin')}`);
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-      );
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
       // console.log('DS',req.path)
       next();
     });
 
     dataServer.get('/users', (req, res) => res.json(users));
-    dataServer.get('/users/:id', (req, res) =>
-      res.json(users.find((row) => row.id === +req.params.id))
-    );
+    dataServer.get('/users/1/raw', (req, res) => {
+      const user = users.find((row) => row.id === 1);
+      res.send(`
+      <h1>Raw sample page</h1>
+      <p>The url "/rawData" is not provided in the sampels app, but comes from an external resource</p>
+      <p> there is some script in here that triggers page-ready<p>
+      <script>
+        /** triggering page ready, as there is no need to wait for a timeout */
+        setTimeout(() => window.dispatchEvent(new Event('AngularReady', {
+          bubbles: true,
+          cancelable: false
+        })),10)
+      </script>
+      `);
+    });
+    dataServer.get('/users/:id', (req, res) => res.json(users.find((row) => row.id === +req.params.id)));
+
     dataServer.get('/posts', (req, res) => {
       const userId = req.query.userId;
       if (userId !== undefined) {
@@ -31,9 +42,7 @@ export async function startDataServer(ssl: boolean) {
       }
       res.json(posts);
     });
-    dataServer.get('/posts/:id', (req, res) =>
-      res.json(posts.find((row) => row.id === +req.params.id))
-    );
+    dataServer.get('/posts/:id', (req, res) => res.json(posts.find((row) => row.id === +req.params.id)));
     dataServer.get('/slow/:delay', (req, res) => {
       const { delay: d } = req.params;
       let delay;
@@ -52,11 +61,7 @@ export async function startDataServer(ssl: boolean) {
       res.send(`<h1>404 - ${req.url}</h1>`);
     });
     return dataServer.listen(8200, scullyConfig.hostName, (x) => {
-      log(
-        `Test data server started on "${yellow(
-          `http://${scullyConfig.hostName}:${8200}/`
-        )}" `
-      );
+      log(`Test data server started on "${yellow(`http://${scullyConfig.hostName}:${8200}/`)}" `);
     });
   } catch (e) {
     /** no need for fancy handling here, its a test data server */
