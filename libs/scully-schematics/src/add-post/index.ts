e4365afcb22c5eec3b4c2d4c6567005c27aa57cd
jorgeucano <jorgeucano@gmail.com>
2020-08-20 11:46:19 -0300

    fix typo

--- libs/scully-schematics/src/add-post/index.ts
+++ libs/scully-schematics/src/add-post/index.ts
@@ -1,48 +1,33 @@
-import {
-  Rule,
-  SchematicContext,
-  SchematicsException,
-  Tree
-} from '@angular-devkit/schematics';
+import { Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
 import { strings } from '@angular-devkit/core';
 
 import { Schema } from './schema';
 import { yamlToJson, jsonToJaml, removeWrongCharacters } from '../utils/utils';
 
-export default function(options: Schema): Rule {
+export default function (options: Schema): Rule {
   return (host: Tree, context: SchematicContext) => {
     const name = options.name;
     const extension = options.extension || 'md';
-    const nameDasherized = options.name
-      ? strings.dasherize(options.name)
-      : 'blog-X';
-    const targetDasherized = options.target
-      ? strings.dasherize(options.target)
-      : 'blog';
+    const nameDasherized = options.name ? strings.dasherize(options.name) : 'blog-X';
+    const targetDasherized = options.target ? strings.dasherize(options.target) : 'blog';
 
     if (!/^[\w]+$/.test(extension)) {
-      throw new SchematicsException(
-        `${extension} is not a valid file extension`
-      );
+      throw new SchematicsException(`${extension} is not a valid file extension`);
     }
 
-    const filename = `./${targetDasherized}/${removeWrongCharacters(
-      nameDasherized
-    )}.${extension}`;
+    const filename = `./${targetDasherized}/${removeWrongCharacters(nameDasherized)}.${extension}`;
 
-    let metaData = {
+    let metaData: any = {
       title: '',
       description: 'blog description',
-      published: false
+      published: false,
     };
 
     if (options.metaDataFile) {
       const metaDataAsJson = yamlToJson(options.metaDataFile);
       if (metaDataAsJson) {
         metaData = metaDataAsJson;
-        context.logger.info(
-          `✅️ Meta Data File ${options.metaDataFile} successfully parsed`
-        );
+        context.logger.info(`✅️ Meta Data File ${options.metaDataFile} successfully parsed`);
       }
     }
 
@@ -59,9 +44,7 @@ ${jsonToJaml(metaData)}---
       context.logger.info(`✅️ Blog ${filename} file created`);
     } else {
       // return name exist
-      throw new SchematicsException(
-        `${nameDasherized} exist in your ${targetDasherized} folder`
-      );
+      throw new SchematicsException(`${nameDasherized} exist in your ${targetDasherized} folder`);
     }
   };
 }
