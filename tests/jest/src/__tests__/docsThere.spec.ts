aba6d36225cad8c902d533aef29b7cbdb3ea44dc
sanderelias <sanderelias+git@gmail.com>
2020-09-02 06:06:40 +0200

    test(monorepo): add title and language attribute testing to docs files

--- tests/jest/src/__tests__/docsThere.spec.ts
+++ tests/jest/src/__tests__/docsThere.spec.ts
@@ -11,12 +11,24 @@ const basePath = join(__dirname, '../../../../');
 
 describe('docsSite', () => {
   const files = getMarkdownFiles(startPath);
+  /**
+   * check if there is HTML for every markdown
+   * and check if all eadings are generated
+   */
   for (const file of files) {
-    const route = converToRoute(file);
+    /** load and parse FM from MD file */
+    const mdContent = readFileSync(file, 'utf-8');
+    const { attributes } = fm(mdContent);
+    const { title, lang, slug } = attributes;
+    const prettyFile = file.replace(basePath, './');
+
+    /** load 'index.html' */
+    const route = (slug ? encodeURIComponent(slug) : file.slice(0, -3)).replace(basePath, '');
     const index = readPage(route, 'doc-sites');
     document.body.parentElement.innerHTML = index;
+
     const headers = Array.from(window.document.querySelectorAll('h1, h2, h3, h4, h5, h6'));
-    const headings = getHeadings(file);
+    const headings = getHeadings(mdContent);
     describe(`Route: "${route}"`, () => {
       for (const heading of headings) {
         it(`should have heading "${heading.textContent}"`, () => {
@@ -25,6 +37,13 @@ describe('docsSite', () => {
         });
       }
     });
+
+    describe(`File "${prettyFile}"`, () => {
+      it('should have the title attribute in its front-matter', () => expect(typeof title).toBe('string'));
+      it('should have at least a length of 2 on the title attribute', () => expect(title.length).toBeGreaterThan(2));
+      it('should have the lang attribute in its front-matter', () => expect(typeof lang).toBe('string'));
+      it('should have a length of 2 on the lang attribute', () => expect(lang.length).toStrictEqual(2));
+    });
   }
 });
 
@@ -32,12 +51,11 @@ describe('docsSite', () => {
  * Read all headings from markdown file, convert them to a dom node
  * exclude ones that have exception in them
  */
-function getHeadings(file) {
+function getHeadings(content: string) {
   const exceptions = ['# angular tutorial', 'my blog post', 'heading 1 ### subheading 1 ## heading 2 ### subheading 2'].map((e) =>
     e.trim().toLowerCase()
   );
-  return readFileSync(file)
-    .toString('utf-8')
+  return content
     .split('\n')
     .map((line) => line.trim())
     .filter((line) => line.startsWith('#') && !exceptions.some((exception) => line.toLowerCase().includes(exception)))
@@ -48,15 +66,6 @@ function getHeadings(file) {
     });
 
   // .map((line) => line.slice(line.indexOf(' ')).trim());
-}
-
-/** calculate the 'file' name from the markdown file or front-matter */
-function converToRoute(file) {
-  file = file.split(basePath).join('');
-  const content = readFileSync(file, 'utf-8');
-  const { attributes } = fm(content);
-  const { slug } = attributes;
-  return '' + (slug ? encodeURIComponent(slug) : file.slice(0, -3));
 }
 
 function getMarkdownFiles(path) {
