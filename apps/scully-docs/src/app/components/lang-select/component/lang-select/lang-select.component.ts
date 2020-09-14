ecd34f6f7b17009ac0ebda0a88fce6ad84f13b73
jorgeucano <jorgeucano@gmail.com>
2020-09-14 09:42:05 -0300

    change lang to tabsa

--- apps/scully-docs/src/app/components/lang-select/component/lang-select/lang-select.component.ts
+++ apps/scully-docs/src/app/components/lang-select/component/lang-select/lang-select.component.ts
@@ -8,20 +8,55 @@ import { CurrentPageLanguageData } from '../../models';
   encapsulation: ViewEncapsulation.None,
   template: `
     <ng-container *ngIf="currentPageLangData$ | async as langData">
-      <input type="checkbox" id="lang-select-checkbox" />
-      <label for="lang-select-checkbox" id="lang-select-label" class="select">
-        <span id="lang-select-text">{{ langData.pageLang }}</span>
-      </label>
-
-      <ul class="lang-select-options">
-        <li *ngFor="let langRoutes of langData.pageLangRoutes">
-          <a [routerLink]="langRoutes.route" routerLinkActive="active" [class.unavailable]="!langData.langRoutes[langRoutes.lang]">
-            {{ langDefaults.labels[langRoutes.lang] }}
-          </a>
-        </li>
-      </ul>
+      <div *ngFor="let langRoutes of langData.pageLangRoutes" class="tab">
+        <button
+          class="tablinks"
+          [routerLink]="langRoutes.route"
+          routerLinkActive="active"
+          [class.unavailable]="!langData.langRoutes[langRoutes.lang]"
+        >
+          {{ langDefaults.labels[langRoutes.lang] }}
+        </button>
+      </div>
     </ng-container>
   `,
+  styles: [
+    `
+      /* Style the tab */
+      .tab {
+        overflow: hidden;
+        border: 1px solid #ccc;
+        background-color: #f1f1f1;
+        border-radius: 8px 8px 0px 0px;
+      }
+
+      /* Style the buttons that are used to open the tab content */
+      .tab button {
+        background-color: inherit;
+        color: rgba(0, 0, 0, 0.7);
+        font-size: 12px;
+        font-weight: 900;
+        float: left;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        padding: 14px 16px;
+        transition: 0.3s;
+      }
+
+      /* Change background color of buttons on hover */
+      .tab button:hover {
+        background-color: #ddd;
+      }
+
+      /* Create an active/current tablink class */
+      .tab button.active {
+        background-color: #ffffff;
+        color: #000000;
+        font-size: 13px;
+      }
+    `,
+  ],
 })
 export class LangSelectComponent {
   langDefaults = this.langSelectService.langDefaults;
