192f60d5106c54253a8bad41c764a890df7aba04
jorgeucano <jorgeucano@gmail.com>
2020-09-24 09:19:11 -0300

    add future blog date in false if published true

--- libs/scully/src/lib/renderPlugins/content-render-utils/readFileAndCheckPrePublishSlug.ts
+++ libs/scully/src/lib/renderPlugins/content-render-utils/readFileAndCheckPrePublishSlug.ts
@@ -27,28 +27,34 @@ export async function readFileAndCheckPrePublishSlug(file) {
   if (meta.hasOwnProperty('publish date') || meta.hasOwnProperty('publish-date') || meta.hasOwnProperty('publishDate')) {
     const date = meta['publish date'] || meta['publish-date'] || meta['publishDate'];
     // check if there is a valid date (getTime on invalid date returns NaN)
-    if (date instanceof Date && date.getTime() === date.getTime() && date.getTime() <= new Date().getTime()) {
-      meta.published = true;
+    if (date instanceof Date && date.getTime() === date.getTime()) {
+      if (date.getTime() <= new Date().getTime()) {
+        meta.published = true;
+      } else {
+        meta.published = false;
+      }
     }
   }
-  if (meta.hasOwnProperty('published') && meta.published === false) {
+  if (meta.hasOwnProperty('published')) {
     /** this post needs an pre-publish slug */
     const slugs = Array.isArray(meta.slugs) ? meta.slugs : [];
     let unPublishedSlug = slugs.find((sl: string) => sl.startsWith(prependString));
-    if (!unPublishedSlug) {
-      /** there is no prepub slug so create one and update the file */
-      unPublishedSlug = createSlug();
-      meta.slugs = slugs.concat(unPublishedSlug);
-      /** string literal, don't format "correctly" or things will break ;) */
-      const newFile = `---
+    if (meta.published === false) {
+      if (!unPublishedSlug) {
+        /** there is no prepub slug so create one and update the file */
+        unPublishedSlug = createSlug();
+        meta.slugs = slugs.concat(unPublishedSlug);
+      }
+      prePublished = true;
+    }
+    /** string literal, don't format "correctly" or things will break ;) */
+    const newFile = `---
 ${stringify(meta)}
 ---
 ${fileContent}`;
-      writeFileSync(file, newFile);
-    }
+    writeFileSync(file, newFile);
     /** overwrite slug from file with prepub only in memory. we don't want a file with the original slug name now. */
     meta.slug = unPublishedSlug;
-    prePublished = true;
   }
   return { meta, fileContent, prePublished };
 }
