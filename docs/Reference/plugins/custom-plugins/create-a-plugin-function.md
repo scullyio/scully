83a48f133220241d4fd4c6135be1922c8fb4aa30
Sander Elias <SanderElias@users.noreply.github.com>
2020-09-25 14:33:44 +0200

    scully(fix) unneeded yaml rewrite (#960)
    * fix(scully): fix mardown head rewrite
    
    * refactor(scully): fix logic so no unneeded file writes happen
    
    * docs(scully): updated some docs

--- docs/Reference/plugins/custom-plugins/create-a-plugin-function.md
+++ docs/Reference/plugins/custom-plugins/create-a-plugin-function.md
@@ -6,3 +6,16 @@ position: 130
 ---
 
 # Create a custom plugin
+
+A plugin is always a function that returns a promise. An async function is an easy way to build one.
+For example, if you want a plugin that does something silly like replacing strings in some piece of HTML you can do it like this:
+
+```typescript
+async function replaceThings(html: string, route: handledRoute) {
+  const replaceStrings = [[':)', '☺']];
+  replaceString.forEach(
+    (find, replace) => (html = html.split(find).join(replace))
+  );
+  return html;
+}
+```
--- libs/scully/src/lib/renderPlugins/content-render-utils/readFileAndCheckPrePublishSlug.ts
+++ libs/scully/src/lib/renderPlugins/content-render-utils/readFileAndCheckPrePublishSlug.ts
@@ -1,7 +1,9 @@
+/* eslint-disable no-prototype-builtins */
 import { readFileSync, writeFileSync } from 'fs';
 import { stringify } from 'yamljs';
 import { randomString } from '../../utils/randomString';
 import { logWarn, yellow } from '../../utils';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fm = require('front-matter');
 
 export interface ContentMetaData {
@@ -28,33 +30,40 @@ export async function readFileAndCheckPrePublishSlug(file) {
     const date = meta['publish date'] || meta['publish-date'] || meta['publishDate'];
     // check if there is a valid date (getTime on invalid date returns NaN)
     if (date instanceof Date && date.getTime() === date.getTime()) {
-      if (date.getTime() <= new Date().getTime()) {
-        meta.published = true;
-      } else {
-        meta.published = false;
+      const published = date.getTime() <= new Date().getTime();
+      if (published && meta.hasOwnProperty('published') && meta.published === false) {
+        /** no need to update when its published anyway already! */
+        updateFileWithNewMeta({ ...meta, published });
       }
+      /** make sure internal published reflects the state of the published date */
+      meta.published = published;
     }
   }
-  if (meta.hasOwnProperty('published')) {
+  if (meta.hasOwnProperty('published') && meta.published === false) {
     /** this post needs an pre-publish slug */
     const slugs = Array.isArray(meta.slugs) ? meta.slugs : [];
     let unPublishedSlug = slugs.find((sl: string) => sl.startsWith(prependString));
-    if (meta.published === false) {
-      if (!unPublishedSlug) {
-        /** there is no prepub slug so create one and update the file */
-        unPublishedSlug = createSlug();
-        meta.slugs = slugs.concat(unPublishedSlug);
-      }
-      prePublished = true;
+    if (!unPublishedSlug) {
+      /** there is no prepub slug so create one and update the file */
+      unPublishedSlug = createSlug();
+      updateFileWithNewMeta({ ...meta, slugs: slugs.concat(unPublishedSlug) });
     }
-    /** string literal, don't format "correctly" or things will break ;) */
-    const newFile = `---
-${stringify(meta)}
----
-${fileContent}`;
-    writeFileSync(file, newFile);
+    prePublished = true;
     /** overwrite slug from file with prepub only in memory. we don't want a file with the original slug name now. */
     meta.slug = unPublishedSlug;
   }
   return { meta, fileContent, prePublished };
+
+  function updateFileWithNewMeta(newMeta: ContentMetaData) {
+    /** only update file on actual changes. */
+    if (JSON.stringify(meta) !== JSON.stringify(newMeta)) {
+      /** string literal, don't format "correctly" or things will break ;) */
+      const newFile = `---
+${stringify(meta).trim()}
+---
+
+${fileContent}`;
+      writeFileSync(file, newFile);
+    }
+  }
 }
