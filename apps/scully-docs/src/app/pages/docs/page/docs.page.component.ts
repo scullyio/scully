c153ce02dad6d4be0a5f091f12552877afe014a6
ngdevelop-tech <aprajapati06@gmail.com>
2020-10-06 21:52:28 +0530

    feat: document suggest edits link

--- apps/scully-docs/src/app/pages/docs/page/docs.page.component.ts
+++ apps/scully-docs/src/app/pages/docs/page/docs.page.component.ts
@@ -8,6 +8,9 @@ import { NavListService } from '../../../components/nav-list/nav-list.service';
   selector: 'scullyio-docs-page',
   encapsulation: ViewEncapsulation.None,
   template: `
+    <div class="github-links" *ngIf="currentPage$ | async as cur">
+      <a class="suggest-edit" [href]="cur.suggestEditLink" aria-label="Suggest Edits" title="Suggest Edits"></a>
+    </div>
     <section class="docs-page-content">
       <scully-content></scully-content>
       <div class="docs-prev_next" *ngIf="currentPage$ | async as cur">
@@ -20,6 +23,9 @@ import { NavListService } from '../../../components/nav-list/nav-list.service';
   `,
 })
 export class DocsPageComponent {
+  githubEditLink = (route: string) =>
+    `https://github.com/scullyio/scully/edit/main${route}.md?message=docs%3A%20describe%20your%20change...`;
+
   currentPage$ = this.nav.currentDoc$.pipe(
     tap((cur) => {
       const title = cur._route?.title as string;
@@ -29,7 +35,7 @@ export class DocsPageComponent {
         this.title.setTitle('Scully Documentation');
       }
     }),
-    map((cur) => ({ next: cur._next, prev: cur._prev })),
+    map((cur) => ({ next: cur._next, prev: cur._prev, suggestEditLink: this.githubEditLink(cur?._route?.route) })),
     /** note, this is for testing only, as in the docs site _no_ code will not be there anyway! */
     catchError((e) => {
       console.error(e);
