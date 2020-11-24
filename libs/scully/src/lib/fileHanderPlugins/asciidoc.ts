311f45ef2ea54e627a237627cf3befbb97c05d99
Sander Elias <SanderElias@users.noreply.github.com>
2020-11-24 16:37:21 +0100

    fix(scully): fix dates getting lost in deepclone (for routeProcessed) (#1140)
    * fix(scully): fix dates getting lost in deepclone (for routeProcessed)
    
    ISSUES CLOSED: y
    
    * fix(scully): fix asciiDocs plugin
    
    ISSUES CLOSED: y

--- libs/scully/src/lib/fileHanderPlugins/asciidoc.ts
+++ libs/scully/src/lib/fileHanderPlugins/asciidoc.ts
@@ -4,17 +4,17 @@ import { logError } from '../utils';
 
 const asciiDocPlugin = async (raw: string) => {
   try {
-    const asciidoctor: any = import('asciidoctor.js');
-    const loggerManager = asciidoctor.LoggerManager;
-    const memoryLogger = asciidoctor.MemoryLogger.create();
-    loggerManager.setLogger(memoryLogger);
+    const asciidoctor: any = require('asciidoctor.js')();
     return asciidoctor.convert(raw, {});
-  } catch {
-    logError(`When you want to use asciidoc, you need to install the optional asciidocor.js module by:
+  } catch (e) {
+    logError(
+      `When you want to use asciidoc, you need to install the optional asciidocor.js module by:
 
   npm install -s asciidoctor.js
 
-`);
+`,
+      e
+    );
   }
 };
 
