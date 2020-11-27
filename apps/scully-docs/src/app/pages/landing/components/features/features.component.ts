1b7c20d73eb1b0296313e9eb78786d0d1458bacc
TeeVenDick <teevendick@gmail.com>
2020-11-27 22:14:15 +0800

    Fix broken link (#1147)

--- apps/scully-docs/src/app/pages/landing/components/features/features.component.ts
+++ apps/scully-docs/src/app/pages/landing/components/features/features.component.ts
@@ -25,7 +25,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
       <div class="feature">
         <div class="icon-container"><img src="/assets/img/icons/puzzle-piece-solid.svg" /></div>
-        <h2><a routerLink="/docs/learn/plugins/overview">Plugin System</a></h2>
+        <h2><a routerLink="/docs/Reference/plugins/overview">Plugin System</a></h2>
         <p>
           Extend and adapt Scully to any need with its powerful plugin ecosystem. Choose from built-in plugins, community plugins,
           or make you own custom plugin and unleash the full power of Scully.
