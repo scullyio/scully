2c710db5a68584193b0e01b8498bbc45af4ca717
sanderelias <sanderelias+git@gmail.com>
2021-01-19 07:50:30 +0100

    fixed repeating tests, move to node 14 for scully

--- jest.preset.js
+++ jest.preset.js
@@ -1,16 +1,4 @@
-const nxPreset = require('@nrwl/jest/preset');
-// module.exports = {
-//   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-//   transform: {
-//     '^.+\\.(ts|js|html)$': 'ts-jest'
-//   },
-//   resolver: '@nrwl/jest/plugins/resolver',
-//   moduleFileExtensions: ['ts', 'js', 'html'],
-//   coverageReporters: ['html']
-// };
 module.exports = {
-  ...nxPreset,
-  // preset: 'jest-preset-angular',
   roots: ['tests/jest/src'],
   transform: {
     '^.+\\.(ts|js|html)$': 'ts-jest',
