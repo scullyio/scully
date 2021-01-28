94e3e6e2606ffafb929c217d6169cbe919c75cb3
sanderelias <sanderelias+git@gmail.com>
2021-01-28 08:45:50 +0100

    fix(docsLib plugin): remove text version that was there for testing purposes only

--- libs/plugins/docs-link-update/src/lib/plugins-docs-link-update.ts
+++ libs/plugins/docs-link-update/src/lib/plugins-docs-link-update.ts
@@ -32,40 +32,8 @@ const docsLinkPlugin = async (dom: JSDOM, options: HandledRoute): Promise<JSDOM>
   return dom;
 };
 
-const docsLinkPluginTxt = async (html: string, options: HandledRoute): Promise<string> => {
-  try {
-    const dom = new JSDOM(html);
-    const { window } = dom;
-    const anchors = Array.from(window.document.querySelectorAll('[href]'));
-    anchors.forEach((a: HTMLAnchorElement) => {
-      const href = a.getAttribute('href');
-      if (href && href.toLowerCase().endsWith('.md') && !href.toLowerCase().startsWith('http')) {
-        let newRef = '';
-        if (!href.startsWith('/')) {
-          const myBase = dropOpeningSlash(options.route.substring(0, options.route.lastIndexOf('/')));
-          newRef = `${myBase}/`;
-        }
-        newRef = newRef + href.slice(0, -3);
-        a.setAttribute('href', newRef);
-      }
-      if (href && href.startsWith('#')) {
-        a.setAttribute('data-hash', href.slice(1));
-        a.setAttribute('href', 'javascript:;');
-        a.setAttribute('onclick', `document.location.hash=this.dataset.hash`);
-        console.log(a.outerHTML);
-      }
-    });
-    return dom.serialize();
-  } catch (e) {
-    logWarn(`error in docsLink, didn't parse for route "${yellow(options.route)}"`);
-  }
-  // in case of failure return unchanged HTML to keep flow going
-  return html;
-};
-
 const validator = async (config) => [];
-registerPlugin('render', docLink, docsLinkPluginTxt, validator);
-// registerPlugin('renderJsDom', docLink, docsLinkPlugin, validator);
+registerPlugin('renderJsDom', docLink, docsLinkPlugin, validator);
 
 function dropEndingSlash(str: string) {
   return str.endsWith('/') ? str.slice(0, -1) : str;
