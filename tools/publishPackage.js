82f9b1b5c645c6648222d52712946c4b3129699c
sanderelias <sanderelias+git@gmail.com>
2021-02-22 16:02:22 +0100

    fix(monorepo): fx wrong path in package.json

---
+++ tools/publishPackage.js
@@ -0,0 +1,31 @@
+import { green, yellow } from 'chalk';
+import { exec } from 'child_process';
+import { join } from 'path';
+import { folder } from './utils';
+export async function publishPackage(tag, toRelease, dryRun = true) {
+  const cmd = `npm publish --access-public --ignore-scripts --tag ${tag}` + (dryRun ? ' --dry-run' : '');
+  const { nodeError, stdOut, stdErr } = await new Promise((resolve, reject) => {
+    // resolve({
+    //   nodeError: undefined,
+    //   stdOut: '',
+    //   stdErr: '',
+    // });
+    exec(cmd, { cwd: join(folder, toRelease.dist) }, (nodeError, stdOut, stdErr) => {
+      resolve({ nodeError, stdOut, stdErr });
+    });
+  });
+  const hasErrror = stdErr?.includes('npm ERR! code');
+  if (hasErrror) {
+    console.log(`
+=========================================
+  Error during npm publish of ${yellow(toRelease.name)}:
+    ${stdErr}
+  This library is not published
+=========================================
+    `);
+    /** fail the process if we cant publish somehow. */
+    // process.exit(15);
+  } else {
+    console.log(`npm released ${green(toRelease.name)} with version ${yellow(toRelease.version)} under the ${yellow(tag)} tag`);
+  }
+}
