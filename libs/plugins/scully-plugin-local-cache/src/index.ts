81687d9113c6c4aba7d301b0d4aa67e3ddea9c26
sanderelias <sanderelias+git@gmail.com>
2021-03-03 07:38:36 +0100

    refactor(scully-plugin-local-cache): export key interfaces
    Those props are in the hash already, but I need to for the vault to be able to separate
    out
    the
    correct items

--- libs/plugins/scully-plugin-local-cache/src/index.ts
+++ libs/plugins/scully-plugin-local-cache/src/index.ts
@@ -1,2 +1,3 @@
 export { localCacheReady, localCache } from './lib/plugins-local-cache';
 export { levelDbReady, del, get, set, initializeLevelDb } from './lib/ldb';
+export { CacheItem, LocalCacheConfig, LookupTable } from './lib/local-cache.interface';
