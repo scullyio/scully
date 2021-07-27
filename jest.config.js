93d555b5b1c34eff326ecf9c3dc0fa4d8d2238c4
Jefiozie <jefiozie.bosch@gmail.com>
2021-07-27 08:46:11 +0200

    chore: update to latest nx 12.6 with angular version 12.1

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,5 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
-  projects: [
-    // '<rootDir>/libs/scully',
-    // '<rootDir>/libs/ng-lib',
-    // '<rootDir>/apps/sample-blog',
-    // '<rootDir>/apps/scully-docs',
-    // '<rootDir>/libs/plugins/extra',
-    '<rootDir>/libs/plugins/from-data',
-    // '<rootDir>/libs/plugins/scully-plugin-local-cache',
-    // '<rootDir>/libs/plugins/base-href-rewrite',
-    // '<rootDir>/libs/plugins/scully-plugin-flash-prevention',
-    // '<rootDir>/libs/plugins/logrocket',
-    // '<rootDir>/libs/plugins/sentry',
-    // '<rootDir>/libs/plugins/google-analytics',
-    // '<rootDir>/libs/plugins/docs-link-update',
-    // '<rootDir>/libs/plugins/scully-plugin-remove-scripts',
-    // '<rootDir>/libs/plugins/scully-plugin-critical-css',
-    // '<rootDir>/libs/plugins/scully-plugin-copy-to-clipboard',
-  ],
+  projects: [...getJestProjects(), '<rootDir>/libs/plugins/from-data'],
 };
