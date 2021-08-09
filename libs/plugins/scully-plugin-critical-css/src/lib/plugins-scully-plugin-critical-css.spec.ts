8a32c5f9aa8224138251a888a18bb8e2cebdc01e
Jefiozie <jefiozie.bosch@gmail.com>
2021-08-09 10:53:59 +0200

    chore: remove all test generated by libs

--- libs/plugins/scully-plugin-critical-css/src/lib/plugins-scully-plugin-critical-css.spec.ts
+++
@@ -1,7 +0,0 @@
-import { pluginsScullyPluginCriticalCss } from './plugins-scully-plugin-critical-css';
-
-describe('pluginsScullyPluginCriticalCss', () => {
-  it('should work', () => {
-    expect(pluginsScullyPluginCriticalCss()).toEqual('plugins-scully-plugin-critical-css');
-  });
-});
