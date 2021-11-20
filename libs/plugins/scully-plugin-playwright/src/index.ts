a3cd6af8c21e76281fd3e3171967649a85634682
Jefiozie <jefiozie.bosch@gmail.com>
2021-11-20 13:19:45 +0100

    fix: run pw deps install before crunning pw

--- libs/plugins/scully-plugin-playwright/src/index.ts
+++ libs/plugins/scully-plugin-playwright/src/index.ts
@@ -1,9 +1,31 @@
-import { findPlugin, logOk, registerPlugin, routeRenderer } from '@scullyio/scully';
+import { findPlugin, log, logError, logOk, registerPlugin, routeRenderer, yellow } from '@scullyio/scully';
+import { exec } from 'child_process';
+import { LaunchOptions } from 'playwright';
 import { playwrightRender } from './lib/plugins-scully-plugin-playwright';
 import { launchedBrowser, launchedBrowser$ } from './lib/plugins-scully-plugin-playwright-utils';
-import { LaunchOptions } from 'playwright'
 
+async function runScript(cmd: string) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        log(stderr);
+        reject(err);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+const plugin = async () => {
+
+  await runScript(`npx playwright install"`).catch(() => {
+    logError(`Couldn't install ${yellow('playwright')} dependencys. Please fix the above errors in the app, and run Scully again.`);
+    process.exit(0);
+  });
+}
 export function enablePW() {
+  registerPlugin('beforeAll', 'installPWDeps', plugin);
+
   registerPlugin('scullySystem', routeRenderer, findPlugin(playwrightRender, undefined, false), undefined, { replaceExistingPlugin: true });
 
   registerPlugin('enterprise', 'getPWLaunchedBrowser', async () => launchedBrowser$)
