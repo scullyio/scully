32d800feb52309b963c5d8f53d899794e4113030
sanderelias <sanderelias+git@gmail.com>
2021-12-17 10:50:32 +0100

    fix(platform-server): make sure ready events doesn't fire premature

--- libs/platform-server/src/lib/platform-server.module.ts
+++ libs/platform-server/src/lib/platform-server.module.ts
@@ -1,6 +1,6 @@
 import { DOCUMENT, XhrFactory } from '@angular/common';
-import { Inject, NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { Inject, NgModule, NgZone } from '@angular/core';
+import { BEFORE_APP_SERIALIZED, ServerModule } from '@angular/platform-server';
 import { ActivatedRoute } from '@angular/router';
 import { IdleMonitorService } from '@scullyio/ng-lib';
 import { ScullyXhrFactory } from './ScullyXhrFactory';
@@ -18,7 +18,10 @@ declare global {
 
 @NgModule({
   imports: [ServerModule],
-  providers: [{ provide: XhrFactory, useClass: ScullyXhrFactory }],
+  providers: [
+    { provide: BEFORE_APP_SERIALIZED, multi: true, useFactory: scullyReadyEventFiredFactory, deps: [DOCUMENT, NgZone] },
+    { provide: XhrFactory, useClass: ScullyXhrFactory },
+  ],
 })
 export class ScullyPlatformServerModule {
   /** make sure it doesn't get optimized away. */
@@ -41,4 +44,37 @@ export class ScullyPlatformServerModule {
       document.body.setAttribute('scully-version', window['scullyVersion']);
     }
   }
+}
+
+/**
+ * Helper that makes sure we don't get the HTML content before the app is ready.
+ * @param doc
+ * @param ngZone
+ * @returns
+ */
+export function scullyReadyEventFiredFactory(doc: Document, ngZone: NgZone): () => Promise<void> {
+  return () =>
+    new Promise((resolve, _reject) => {
+      let fired = false;
+      ngZone.runOutsideAngular(() => {
+        const monitor = () => {
+          /** check if there are still macrotasks. If so keep polling */
+          if (ngZone.hasPendingMacrotasks) {
+            // logWarn(`pending tasks.`);
+            return setTimeout(monitor, 25);
+          }
+          /** also keep polling when Scully isn't ready yet */
+          if (!fired) {
+            return setTimeout(monitor, 25);
+          }
+          /** allow for at least 1 cycle after both above are green. */
+          return setTimeout(() => resolve(), 5);
+        };
+        monitor();
+        /** we need at least the Scully generated event. It does fire _too_ soon under Platform serer, but it signals all things Scully are _go_ */
+        doc.addEventListener('AngularReady', () => {
+          fired = true;
+        });
+      });
+    });
 }
